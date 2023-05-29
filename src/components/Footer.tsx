397fd458f9af71f37ebca1883a93ea5d25f3798c
Marcus McBride <marcusmcb@gmail.com>
2023-05-29 16:09:57 -0700

    updated images for main portfolio view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,62 +1,61 @@
-import styles from "./styles/Footer.module.css";
-import logo from "../images/mcb-logo.jpg";
-import { Link } from "react-router-dom";
-import github from "../images/icons/github.svg";
-import twitter from "../images/icons/twitter.svg";
-import linkedin from "../images/icons/linkedin.svg";
+import styles from './styles/Footer.module.css'
+import logo from '../images/mcb-logo.jpg'
+import { Link } from 'react-router-dom'
+import github from '../images/icons/github.svg'
+import twitter from '../images/icons/twitter.svg'
+import linkedin from '../images/icons/linkedin.svg'
 
 const Footer = (): JSX.Element => {
-  return (
-    <footer>
-      <div className={`container ${styles.container}`}>
-        <div className={styles.imageContainer} style={{ cursor: "pointer" }}>
-          <img src={logo} alt="logo" className={styles.image} />
-        </div>
-        <nav id="bottomnav" className={styles.nav}>
-          <Link to="/" className={styles.item}>
-            HOME
-          </Link>
-          <Link to="/portfolio" className={styles.item}>
-            PORTFOLIO
-          </Link>
-          <Link to="/contact" className={styles.item}>
-            CONTACT
-          </Link>
-        </nav>
-        <div className={styles.iconContainer}>
-          <a
-            href="https://github.com/marcusmcb"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.iconButton}
-            aria-label="github"
-          >
-            <img src={github} alt="github" className={styles.icon} />
-          </a>
+	return (
+		<footer>
+			<div className={`container ${styles.container}`}>
+				<div className={styles.imageContainer} style={{ cursor: 'pointer' }}>
+					<img src={logo} alt='logo' className={styles.image} />
+				</div>
+				<nav id='bottomnav' className={styles.nav}>
+					<Link to='/' className={styles.item}>
+						HOME
+					</Link>
+					<Link to='/portfolio' className={styles.item}>
+						PORTFOLIO
+					</Link>
+					<Link to='/contact' className={styles.item}>
+						CONTACT
+					</Link>
+				</nav>
+				<div className={styles.iconContainer}>
+					<a
+						href='https://www.linkedin.com/in/marcusmcbride-developer/'
+						target='_blank'
+						rel='noreferrer'
+						className={styles.iconButton}
+						aria-label='linkedin'
+					>
+						<img src={linkedin} alt='linkedin' className={styles.icon} />
+					</a>
+					<a
+						href='https://github.com/marcusmcb'
+						target='_blank'
+						rel='noreferrer'
+						className={styles.iconButton}
+						aria-label='github'
+					>
+						<img src={github} alt='github' className={styles.icon} />
+					</a>
 
-          <a
-            href="https://www.twitter.com/djmarcusmcb"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.iconButton}
-            aria-label="twitter"
-          >
-            <img src={twitter} alt="twitter" className={styles.icon} />
-          </a>
+					<a
+						href='https://www.twitter.com/djmarcusmcb'
+						target='_blank'
+						rel='noreferrer'
+						className={styles.iconButton}
+						aria-label='twitter'
+					>
+						<img src={twitter} alt='twitter' className={styles.icon} />
+					</a>
+				</div>
+			</div>
+		</footer>
+	)
+}
 
-          <a
-            href="https://www.linkedin.com/in/marcusmcbride-developer/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.iconButton}
-            aria-label="linkedin"
-          >
-            <img src={linkedin} alt="linkedin" className={styles.icon} />
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+export default Footer
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,147 +1,182 @@
-import styles from "./styles/Portfolio.module.css";
-import { useEffect } from "react";
-import SectionThree from "../homepage/SectionThree";
-import Project from "./Project";
+import styles from './styles/Portfolio.module.css'
+import { useEffect } from 'react'
+import SectionThree from '../homepage/SectionThree'
+import Project from './Project'
 
-import manageMobil from "../../images/portfolio/crate_stats_01.png";
-import manageTablet from "../../images/portfolio/crate_stats_01.png";
-import manageDesktop from "../../images/portfolio/crate_stats_01.png";
+import cratestatsMobile from '../../images/portfolio/cratestats/cratestats_preview.png'
+import cratestatsTablet from '../../images/portfolio/cratestats/cratestats_preview.png'
+import cratestatsDesktop from '../../images/portfolio/cratestats/cratestats_preview.png'
 
-import bookmarkMobil from "../../images/portfolio/mobile/image-portfolio-bookmark@2x.jpg";
-import bookmarkTablet from "../../images/portfolio/tablet/image-portfolio-bookmark@2x.jpg";
-import bookmarkDesktop from "../../images/portfolio/desktop/image-portfolio-bookmark@2x.jpg";
+import npchatbotMobile from '../../images/portfolio/npchatbot/npchatbot_preview.png'
+import npchatbotTablet from '../../images/portfolio/npchatbot/npchatbot_preview.png'
+import npchatbotDesktop from '../../images/portfolio/npchatbot/npchatbot_preview.png'
 
-import fyloMobil from "../../images/portfolio/mobile/image-portfolio-fylo@2x.jpg";
-import fyloTablet from "../../images/portfolio/tablet/image-portfolio-fylo@2x.jpg";
-import fyloDesktop from "../../images/portfolio/desktop/image-portfolio-fylo@2x.jpg";
+import fyloMobil from '../../images/portfolio/mobile/image-portfolio-fylo@2x.jpg'
+import fyloTablet from '../../images/portfolio/tablet/image-portfolio-fylo@2x.jpg'
+import fyloDesktop from '../../images/portfolio/desktop/image-portfolio-fylo@2x.jpg'
 
-import remixarkiveMobile from "../../images/portfolio/remix_arkive_screenshot.png";
-import remixarkiveTablet from "../../images/portfolio/remix_arkive_screenshot.png";
-import remixarkiveDesktop from "../../images/portfolio/remix_arkive_screenshot.png";
+import hueMobile from '../../images/portfolio/hue/hue_preview.png'
+import hueTablet from '../../images/portfolio/hue/hue_preview.png'
+import hueDesktop from '../../images/portfolio/hue/hue_preview.png'
 
-import jsp1Mobile from "../../images/portfolio/jsp1/jsp1.png";
-import jsp1Tablet from "../../images/portfolio/jsp1/jsp1.png";
-import jsp1Desktop from "../../images/portfolio/jsp1/jsp1.png";
+import remixarkiveMobile from '../../images/portfolio/remixarkive/remix_arkive_01.png'
+import remixarkiveTablet from '../../images/portfolio/remixarkive/remix_arkive_01.png'
+import remixarkiveDesktop from '../../images/portfolio/remixarkive/remix_arkive_01.png'
+
+import twitchbotMobile from '../../images/portfolio/twitchbot/twitchbot_preview.png'
+import twitchbotTablet from '../../images/portfolio/twitchbot/twitchbot_preview.png'
+import twitchbotDesktop from '../../images/portfolio/twitchbot/twitchbot_preview.png'
+
+import rentstudyMobile from '../../images/portfolio/rentstudy/rentstudy_preview.png'
+import rentstudyTablet from '../../images/portfolio/rentstudy/rentstudy_preview.png'
+import rentstudyDesktop from '../../images/portfolio/rentstudy/rentstudy_preview.png'
+
+import jsp1Mobile from '../../images/portfolio/jsp1/jsp1_preview.png'
+import jsp1Tablet from '../../images/portfolio/jsp1/jsp1_preview.png'
+import jsp1Desktop from '../../images/portfolio/jsp1/jsp1_preview.png'
+
+import responsiveMobile from '../../images/portfolio/responsive/responsive_preview.png'
+import responsiveTablet from '../../images/portfolio/responsive/responsive_preview.png'
+import responsiveDesktop from '../../images/portfolio/responsive/responsive_preview.png'
 
 export default function Portfolio(): JSX.Element {
-  useEffect(() => {
-    document.title = "MCB Portfolio | Projects";
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <main className={styles.main}>
-      <Project.Right
-        title="CrateStats"
-        description={
-          <>
-            CrateStats is a data analytics platform currently in development.
-            The goal of this project is to give the DJ community a dedicated
-            resource for playlist analysis and performance insight across
-            multiple DVS platforms. <br />
-            <br />
-            Support for Serato, Serato Live Playlists, Traktor, and Rekordbox is
-            currently included. Built with React, Material UI, Node, and GCP.
-          </>
-        }
-        screenshots={[manageMobil, manageTablet, manageDesktop]}
-        param="cratestats"
-      />
-      <Project.Left
-        title="npChatbot"
-        description={
-          <>
-            This chat-bot adds a set of command features to your Twitch streams
-            that allow viewers to interact with your Serato live playlist (via
-            web scrape). Features within the bot allow viewers to display the
-            current track playing, any previous track played, the total
-            number of tracks played so far, along with a "vibe check" which returns a random
-            selection from the play history and when it was played.
-            <br />
-            <br />
-            Built with Node, TMI.js, Axios/Cheerio, and Electron.
-          </>
-        }
-        screenshots={[bookmarkMobil, bookmarkTablet, bookmarkDesktop]}
-        param="npchatbot"
-      />
-      <Project.Right
-        title="RemixArkive"
-        description={
-          <>
-            RemixArkive is an automated script that uses Twitter to tweet out
-            links to rare, unusual, or hard-to-find remixed versions of songs.
-            <br />
-            <br />
-            Built with Node, Heroku, and the Twitter API.
-          </>
-        }
-        screenshots={[remixarkiveMobile, remixarkiveTablet, remixarkiveDesktop]}
-        param="remixarkive"
-      />
-      <Project.Left
-        title="Philips Hue Smart Lighting Control"
-        description={
-          <>
-            This is a chat-bot script that integrates with the Twitch platform
-            to offer viewers an array of chat commands to control any Hue smart
-            lighting devices that the streamer currently has connected. The
-            script requires at least one Hue fixture as well the accompanying
-            Bridge component and can be configured to run locally or online via
-            proxy.
-            <br />
-            <br />
-            Built with Node, TMI.js, and the Phillips-Hue API.
-          </>
-        }
-        screenshots={[fyloMobil, fyloTablet, fyloDesktop]}
-        param="hue"
-      />
-      <Project.Right
-        title="JSP-1: Javascript Sample Player"
-        description={
-          <>
-            I watched an excellent{" "}
-            <span>
-              <a href="https://www.youtube.com/watch?v=VuN8qwZoego" rel="noreferrer" target="blank">
-                YouTube
-              </a>
-            </span>{" "}
-            tutorial awhile back on how to build a simple QWERTY drum machine using
-            Javascript. I decided to add a few additional sound kits, MIDI
-            connectivity, and even velocity sensitivity for added MIDI playability. It's pretty cool.
-            <br />
-            <br />
-            Built with good old vanilla Javascript.
-          </>
-        }
-        screenshots={[jsp1Mobile, jsp1Tablet, jsp1Desktop]}
-        param="jsp"
-      />
-      <Project.Left
-        title="Twitch Chat Bot"
-        description="
-        This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive.
-         I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes."
-        screenshots={[fyloMobil, fyloTablet, fyloDesktop]}
-        param="twitchchatbot"
-      />
-      <Project.Right
-        title="Responsive Components"
-        description="
-        This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive.
-         I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes."
-        screenshots={[fyloMobil, fyloTablet, fyloDesktop]}
-        param="responsive-components"
-      />
-      <Project.Left
-        title="US Rental Pricing Analysis"
-        description="
-        This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive.
-         I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes."
-        screenshots={[fyloMobil, fyloTablet, fyloDesktop]}
-        param="rental-analysis"
-      />
-      <SectionThree />
-    </main>
-  );
+	useEffect(() => {
+		document.title = 'MCB Portfolio | Projects'
+		window.scrollTo(0, 0)
+	}, [])
+	return (
+		<main className={styles.main}>
+			<Project.Right
+				title='CrateStats'
+				description={
+					<>
+						CrateStats is a data analytics platform currently in development.
+						The goal of this project is to give the DJ community a dedicated
+						resource for playlist analysis and performance insight across
+						multiple DVS platforms. <br />
+						<br />
+						Support for Serato, Serato Live Playlists, Traktor, and Rekordbox is
+						currently included. Built with React, Material UI, Node, and GCP.
+					</>
+				}
+				screenshots={[cratestatsMobile, cratestatsTablet, cratestatsDesktop]}
+				param='cratestats'
+			/>
+			<Project.Left
+				title='npChatbot'
+				description={
+					<>
+						This chat-bot adds a set of command features to your Twitch streams
+						that allow viewers to interact with your Serato live playlist (via
+						web scrape). Features within the bot allow viewers to display the
+						current track playing, any previous track played, the total number
+						of tracks played so far, along with a "vibe check" which returns a
+						random selection from the play history and when it was played.
+						<br />
+						<br />
+						Built with Node, TMI.js, Axios/Cheerio, and Electron.
+					</>
+				}
+				screenshots={[npchatbotMobile, npchatbotTablet, npchatbotDesktop]}
+				param='npchatbot'
+			/>
+			<Project.Right
+				title='RemixArkive'
+				description={
+					<>
+						RemixArkive is an automated script that uses Twitter to tweet out
+						links to rare, unusual, or hard-to-find remixed versions of songs.
+						<br />
+						<br />
+						Built with Node, Heroku, and the Twitter API.
+					</>
+				}
+				screenshots={[remixarkiveMobile, remixarkiveTablet, remixarkiveDesktop]}
+				param='remixarkive'
+			/>
+			<Project.Left
+				title='Philips Hue Smart Lighting Control'
+				description={
+					<>
+						This is a chat-bot script that integrates with the Twitch platform
+						to offer viewers an array of chat commands to control any Hue smart
+						lighting devices that the streamer currently has connected. The
+						script requires at least one Hue fixture as well the accompanying
+						Bridge component and can be configured to run locally or online via
+						proxy.
+						<br />
+						<br />
+						Built with Node, TMI.js, and the Phillips-Hue API.
+					</>
+				}
+				screenshots={[hueMobile, hueTablet, hueDesktop]}
+				param='hue'
+			/>
+			<Project.Right
+				title='JSP-1: Javascript Sample Player'
+				description={
+					<>
+						I watched an excellent{' '}
+						<span>
+							<a
+								href='https://www.youtube.com/watch?v=VuN8qwZoego'
+								rel='noreferrer'
+								target='blank'
+							>
+								YouTube
+							</a>
+						</span>{' '}
+						tutorial awhile back on how to build a simple QWERTY drum machine
+						using Javascript. I decided to add a few additional sound kits, MIDI
+						connectivity, and even velocity sensitivity for added MIDI
+						playability. It's pretty cool.
+						<br />
+						<br />
+						Built with good old vanilla Javascript.
+					</>
+				}
+				screenshots={[jsp1Mobile, jsp1Tablet, jsp1Desktop]}
+				param='jsp'
+			/>
+			<Project.Left
+				title='Twitch Chat Bot'
+				description={
+					<>
+						This is a custom chatbot solution that I built for my own Twitch
+						channel. It features a number of API-connected commands,
+						user-interactive games (Magic 8-Ball, Rock-Paper-Scissors), and
+						command "cool-off" feature for to prevent viewer overuse. Built with
+						Node, TMI.js, and the Twitch API.
+					</>
+				}
+				screenshots={[twitchbotMobile, twitchbotTablet, twitchbotDesktop]}
+				param='twitchchatbot'
+			/>
+			<Project.Right
+				title='Responsive Components'
+				description={
+					<>
+						Here you'll find a collection of mobile-first, responsive UI
+						components and elements that I've built using Javascript, React, and CSS.
+					</>
+				}
+				screenshots={[responsiveMobile, responsiveTablet, responsiveDesktop]}
+				param='responsive-components'
+			/>
+			<Project.Left
+				title='US Rental Pricing Analysis'
+				description={
+					<>
+						As part of a student data science project at UC Irvine in 2019, my
+						team mates compiled a series of slides with data collected, cleaned,
+						and presented regarding US rental pricing trends. Built with Python,
+						Pandas/Numpy, Plotly, and the Google Maps API.
+					</>
+				}
+				screenshots={[rentstudyMobile, rentstudyTablet, rentstudyDesktop]}
+				param='rental-analysis'
+			/>
+			<SectionThree />
+		</main>
+	)
 }
