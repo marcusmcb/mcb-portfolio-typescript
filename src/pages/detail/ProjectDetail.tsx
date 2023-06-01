7f16b88ec715864f177a9bc6dcb6b643020be6ff
Marcus McBride <marcusmcb@gmail.com>
2023-06-01 16:18:28 -0700

    updated logic on proj detail preview images, working

--- src/pages/detail/ProjectDetail.tsx
+++ src/pages/detail/ProjectDetail.tsx
@@ -1,127 +1,133 @@
-import { useEffect } from "react";
-import SecondaryButton from "../../components/SecondaryButton";
-import ExternalButton from "../../components/ExternalButton";
-import Navigation from "./Navigation";
-import styles from "./styles/ProjectDetail.module.css";
+import { useEffect } from 'react'
+import SecondaryButton from '../../components/SecondaryButton'
+import ExternalButton from '../../components/ExternalButton'
+import Navigation from './Navigation'
+import styles from './styles/ProjectDetail.module.css'
 
 interface ProjectDetailProps {
-  heroImages: string[];
-  title: string;
-  description: any;
-  keywords: string[];
-  technologies: string[];
-  background: any;
-  link: string[];
-  preview1: string[];
-  preview2: string[];
-  next: string;
-  previous: string;
-  external_link?: string[];
+	heroImages: string[]
+	title: string
+	description: any
+	keywords: string[]
+	technologies: string[]
+	background: any
+	link: string[]
+	preview1: string[]
+	preview2?: string[]
+	next: string
+	previous: string
+	external_link?: string[]
 }
 
 const ProjectDetail = ({
-  heroImages,
-  title,
-  description,
-  keywords,
-  technologies,
-  background,
-  link,
-  preview1,
-  preview2,
-  next,
-  previous,
-  external_link,
+	heroImages,
+	title,
+	description,
+	keywords,
+	technologies,
+	background,
+	link,
+	preview1,
+	preview2,
+	next,
+	previous,
+	external_link,
 }: ProjectDetailProps): JSX.Element => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [])
 
-  console.log(link[0]);
+	console.log(link[0])
 
-  return (
-    <>
-      <section className={styles.sectionOne}>
-        <div className="container">
-          <picture>
-            <source srcSet={heroImages[2]} media="(min-width: 768px)" />
-            <source srcSet={heroImages[1]} media="(min-width: 768px)" />
-            <img src={heroImages[0]} alt="hero screenshot" width="100%" />
-          </picture>
-        </div>
-      </section>
+	return (
+		<>
+			<section className={styles.sectionOne}>
+				<div className='container'>
+					<picture>
+						<source srcSet={heroImages[2]} media='(min-width: 768px)' />
+						<source srcSet={heroImages[1]} media='(min-width: 768px)' />
+						<img src={heroImages[0]} alt='hero screenshot' width='100%' />
+					</picture>
+				</div>
+			</section>
 
-      <section className={styles.sectionTwo}>
-        <div className={`container ${styles.container}`}>
-          {/* left container */}
-          <div className={styles.blockContainer}>
-            <h2 className={`h2 ${styles.title}`} style={{ textAlign: "start" }}>
-              {title}
-            </h2>
-            <p
-              className={`body-2 ${styles.description}`}
-              style={{ textAlign: "start", opacity: "0.8" }}
-            >
-              {description}
-            </p>
-            <p className={styles.keywords}>
-              {keywords.map((element, index) => {
-                if (index === keywords.length - 1) {
-                  return element;
-                } else {
-                  return `${element} / `;
-                }
-              })}
-            </p>
-            <p className={styles.words}>
-              {technologies.map((element, index) => {
-                if (index === technologies.length - 1) {
-                  return element;
-                } else {
-                  return `${element} | `;
-                }
-              })}
-            </p>
-            {external_link ? (
-              <ExternalButton
-                text="external website"
-                href={link[0]}
-                rel={"noreferrer"}
-                target={"_blank"}
-              />
-            ) : (
-              <SecondaryButton text="visit website" href={link[0]} />
-            )}
-			{/* <SecondaryButton text="visit website" href={link[0]} /> */}
-          </div>
+			<section className={styles.sectionTwo}>
+				<div className={`container ${styles.container}`}>
+					{/* left container */}
+					<div className={styles.blockContainer}>
+						<h2 className={`h2 ${styles.title}`} style={{ textAlign: 'start' }}>
+							{title}
+						</h2>
+						<p
+							className={`body-2 ${styles.description}`}
+							style={{ textAlign: 'start', opacity: '0.8' }}
+						>
+							{description}
+						</p>
+						<p className={styles.keywords}>
+							{keywords.map((element, index) => {
+								if (index === keywords.length - 1) {
+									return element
+								} else {
+									return `${element} / `
+								}
+							})}
+						</p>
+						<p className={styles.words}>
+							{technologies.map((element, index) => {
+								if (index === technologies.length - 1) {
+									return element
+								} else {
+									return `${element} | `
+								}
+							})}
+						</p>
+						{external_link ? (
+							<div style={{ marginBottom: '15px' }}>
+								<ExternalButton
+									text='visit website'
+									href={link[0]}
+									rel={'noreferrer'}
+									target={'_blank'}
+								/>
+							</div>
+						) : (
+							<></>
+						)}
+						<SecondaryButton text='browse code' href={link[0]} />
+					</div>
 
-          {/* right container */}
-          <div className={styles.backgroundContainer}>
-            <h3 className={`h3 ${styles.bTitle}`}>Project Background</h3>
-            <p className={`body-2 ${styles.background}`}>{background}</p>
-            {/* <h3 className={`h3 ${styles.staticTitle}`}>Static Previews</h3> */}
-            {/* <div>{previews}</div> */}
-            <picture>
-              <source srcSet={preview1[2]} media="(min-width: 1440px)" />
-              <source srcSet={preview1[1]} media="(min-width: 768px)" />
-              <img
-                src={preview1[0]}
-                alt="hero screenshot"
-                width="100%"
-                style={{ marginBottom: "40px" }}
-              />
-            </picture>
-            <picture>
-              <source srcSet={preview2[2]} media="(min-width: 1440px)" />
-              <source srcSet={preview2[1]} media="(min-width: 768px)" />
-              <img src={preview2[0]} alt="hero screenshot" width="100%" />
-            </picture>
-          </div>
-        </div>
-      </section>
-      <Navigation next={next} previous={previous} />
-    </>
-  );
-};
+					{/* right container */}
+					<div className={styles.backgroundContainer}>
+						<h3 className={`h3 ${styles.bTitle}`}>Project Background</h3>
+						<p className={`body-2 ${styles.background}`}>{background}</p>
+						<h3 className={`h3 ${styles.staticTitle}`}>Previews:</h3>
 
-export default ProjectDetail;
+						<picture>
+							<source srcSet={preview1[2]} media='(min-width: 1440px)' />
+							<source srcSet={preview1[1]} media='(min-width: 768px)' />
+							<img
+								src={preview1[0]}
+								alt='hero screenshot'
+								width='100%'
+								style={{ marginBottom: '40px' }}
+							/>
+						</picture>
+						{preview2 ? (
+							<picture>
+								<source srcSet={preview2[2]} media='(min-width: 1440px)' />
+								<source srcSet={preview2[1]} media='(min-width: 768px)' />
+								<img src={preview2[0]} alt='hero screenshot' width='100%' />
+							</picture>
+						) : (
+							<></>
+						)}
+					</div>
+				</div>
+			</section>
+			<Navigation next={next} previous={previous} />
+		</>
+	)
+}
+
+export default ProjectDetail
--- src/pages/detail/TwitchBot.tsx
+++ src/pages/detail/TwitchBot.tsx
@@ -17,10 +17,21 @@ const TwitchBot = (): JSX.Element => {
 		<ProjectDetail
 			heroImages={[heroMobile, heroTablet, heroDesktop]}
 			title='Twitch Bot'
-			description={<>Twitch Chat Bot Description</>}
-			keywords={['Live Streaming', 'Twitch', 'Chat Bot']}
-			technologies={['Javascript', 'Node', 'TMI.js']}
+			description={
+				<>
+					This is a custom chatbot solution that I built for my own Twitch
+					channel. It features a number of API-connected commands (dad jokes,
+					quotes, weather look-up), user-interactive games (Magic 8-Ball,
+					Rock-Paper-Scissors), and a command "cool-off" feature to prevent
+					viewer overuse/abuse. <br />
+					<br />
+					Built with Node, TMI.js, and the Twitch API.
+				</>
+			}
+			keywords={['Live Streaming', 'Chat Bot', 'Interactive']}
+			technologies={['Javascript', 'Node', 'Twitch API', 'TMI.js']}
 			link={['https://www.twitch.tv/djmarcusmcb']}
+			external_link={['https://www.twitch.tv/djmarcusmcb']}
 			background={<>Twitch Chat Bot Background</>}
 			preview1={[previewMobile1, previewTablet1, previewDesktop1]}
 			preview2={[previewMobile2, previewTablet2, previewDesktop2]}
