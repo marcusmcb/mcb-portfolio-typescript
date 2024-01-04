a2c4a242f721a4a67309e633db16f0d5ac385486
marcusmcb <marcusmcb@gmail.com>
2024-01-03 18:15:44 -0800

    updated resume PDF link

--- src/pages/contact/GetInTouch.tsx
+++ src/pages/contact/GetInTouch.tsx
@@ -72,7 +72,7 @@ const GetInTouch = (): JSX.Element => {
 								style={{ opacity: '0.8', fontWeight: '600' }}
 							>
 								<a
-									href='https://drive.google.com/file/d/1J0CN-M3s8EkPhE3hndyWG5R9-y52yLCh/view?usp=sharing'
+									href='https://drive.google.com/file/d/1f884d5Axc2_lbtr-H3VlJRblIx45XwN9/view?usp=sharing'
 									rel='noreferrer'
 									target='_blank'
 									className='resume-link'
