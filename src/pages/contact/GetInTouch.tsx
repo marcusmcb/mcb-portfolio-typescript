56a73c43e04a6d78c75eaaecca3be6a96b7466ae
marcusmcb <marcusmcb@gmail.com>
2025-04-23 11:39:14 -0700

    updated google drive PDF link

--- src/pages/contact/GetInTouch.tsx
+++ src/pages/contact/GetInTouch.tsx
@@ -73,7 +73,7 @@ const GetInTouch = (): JSX.Element => {
 								style={{ opacity: '0.8', fontWeight: '600' }}
 							>
 								<a
-									href='https://drive.google.com/file/d/1mBdVPxWk1M0_aT1sRUiRZA8ddonhZfx3/view?usp=sharing'
+									href='https://drive.google.com/file/d/1z5GgR9nk-rHvaPGsfHKSG7YitIgAJCgI/view?usp=sharing'
 									rel='noreferrer'
 									target='_blank'
 									className='resume-link'
